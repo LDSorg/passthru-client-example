a3ca923f091fc5a2a8bfc59f1e1c5817839456f2
AJ ONeal <awesome@coolaj86.com>
2014-12-30 01:23:48 -0700

    don't test init

--- token-exchange.js
+++ token-exchange.js
@@ -9,47 +9,38 @@ var PromiseA = require('bluebird').Promise
   ;
 
 requestAsync({
-  url: testConfig.proxyUrl + '/api/init'
+  url: testConfig.proxyUrl + '/api/login'
 , method: 'POST'
-, json: { secret: testConfig.secret }
+, json: { username: testConfig.username, password: testConfig.password }
 , agent: testAgent
 }).spread(function (resp, body) {
+  console.log('sign-in result');
   console.log(body);
 
+  if (!body.token) {
+    throw new Error('Invalid Credentials');
+  }
+
   return requestAsync({
-    url: testConfig.proxyUrl + '/api/login'
+    url: testConfig.proxyUrl + '/api/passthru'
   , method: 'POST'
-  , json: { username: testConfig.username, password: testConfig.password }
+  , json: { token: body.token }
   , agent: testAgent
   }).spread(function (resp, body) {
-    console.log('sign-in result');
-    console.log(body);
-
-    if (!body.token) {
-      throw new Error('Invalid Credentials');
+    if (!body.jar) {
+      throw new Error('Token Credentials');
     }
 
+    console.log('token exchange result');
+    var jar = JarSON.fromJSON(body.jar)
+      ;
+
     return requestAsync({
-      url: testConfig.proxyUrl + '/api/passthru'
-    , method: 'POST'
-    , json: { token: body.token }
-    , agent: testAgent
+      url: 'https://www.lds.org/directory/services/ludrs/mem/current-user-info/'
+    , jar: jar
     }).spread(function (resp, body) {
-      if (!body.jar) {
-        throw new Error('Token Credentials');
-      }
-
-      console.log('token exchange result');
-      var jar = JarSON.fromJSON(body.jar)
-        ;
-
-      return requestAsync({
-        url: 'https://www.lds.org/directory/services/ludrs/mem/current-user-info/'
-      , jar: jar
-      }).spread(function (resp, body) {
-        console.log('access result');
-        console.log(body);
-      });
+      console.log('access result');
+      console.log(body);
     });
   });
 });
