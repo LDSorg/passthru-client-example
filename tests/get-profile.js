a479634e3d6692d4585bac13f3566b947ecc7721
AJ ONeal <awesome@coolaj86.com>
2015-03-08 12:02:59 -0600

    Update get-profile.js

--- tests/get-profile.js
+++ tests/get-profile.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var PromiseA = require('bluebird').Promise
-  , request = require('request')
-  , requestAsync = PromiseA.promisify(request)
-  , testConfig = require('../config')
-  , JarSON = require('../jarson')
-  , testAgent = require('../test-agent')
-  ;
+var PromiseA = require('bluebird').Promise;
+var request = require('request');
+var requestAsync = PromiseA.promisify(request);
+var testConfig = require('../config');
+var JarSON = require('jarson');
+var testAgent = require('../test-agent');
 
 requestAsync({
     url: testConfig.proxyUrl + '/api/login'
@@ -19,8 +18,7 @@ requestAsync({
       console.error(body.error);
       return;
     }
-    var jar = JarSON.fromJSON(body.jar)
-      ;
+    var jar = JarSON.fromJSON(body.jar);
 
     return requestAsync({
       url: 'https://www.lds.org/directory/services/ludrs/mem/current-user-info/'
