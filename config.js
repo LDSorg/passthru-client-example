9f04dee4e1e0af68ee57f15593c33ee4be5f159a
AJ ONeal <awesome@coolaj86.com>
2015-03-08 11:58:20 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
 'use strict';
 
-var path = require('path')
-  , fqdn = 'local.foobar3000.com'
-  ;
+var path = require('path');
+var fqdn = 'local.foobar3000.com';
+var port = 8043;
 
 module.exports = {
-  username: require('./real-secret.js').username
-, password: require('./real-secret.js').password
-, proxyUrl: 'https://' + fqdn + ':8043'
+  proxyUrl: 'https://' + fqdn + ':' + port
 , hostname: fqdn
-, port: '8043'
+, port: port
 , ca: path.join(__dirname, 'certs', 'ca', 'my-root-ca.crt.pem')
 , key: path.join(__dirname, 'certs', 'client', 'my-app-client.key.pem')
 , cert: path.join(__dirname, 'certs', 'client', 'my-app-client.crt.pem')
